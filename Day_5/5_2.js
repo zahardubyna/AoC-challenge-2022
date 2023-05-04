9f0fc85c12d5cf693d82d56991abd471eea5a772
ZaHaR <104730836+ZaHaRyK@users.noreply.github.com>
2023-05-04 19:42:18 +0300

    Add files via upload

---
+++ Day_5/5_2.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+let input = fs.readFileSync('req.txt','utf-8')
+const arr = input.split('\n').map(item => item.match(/\d+/g).map(Number))
+
+let box = [
+    ['G', 'T', 'R','W'],
+    ['G', 'C', 'H', 'P', 'M', 'S', 'V', 'W'],
+    ['C', 'L', 'T', 'S', 'G', 'M'],
+    ['J', 'H', 'D', 'M', 'W', 'R', 'F'],
+    ['P', 'Q', 'L', 'H', 'S', 'W', 'F', 'G'],
+    ['P', 'J', 'D', 'N', 'F', 'M', 'S'],
+    ['Z', 'B', 'D', 'F', 'G', 'C', 'S', 'J'],
+    ['R', 'T', 'B'],
+    ['H', 'N', 'W', 'L', 'C'],
+]
+
+for (let i = 0; i < arr.length; i++) {
+    let letterCount = arr[i][0]
+    let sourceArrayIndex = arr[i][1] - 1;
+    let destinationArrayIndex = arr[i][2] - 1;
+      
+    let letter = box[sourceArrayIndex].splice(box[sourceArrayIndex].length - letterCount, letterCount);
+    box[destinationArrayIndex].push(...letter);
+}
+
+console.log(box.map(item => item[item.length - 1]))
+// LVMRWSSPZ
