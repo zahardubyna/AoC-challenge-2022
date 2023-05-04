b02e6218584b56e455ba27b995832385bdd5f638
ZaHaR <104730836+ZaHaRyK@users.noreply.github.com>
2023-05-04 19:40:47 +0300

    Update 4_1.js

--- Day_4/4_1.js
+++ Day_4/4_1.js
@@ -4,14 +4,14 @@ const arr = fs.readFileSync("req.txt", "utf8").split('\r\n').map(item => item.sp
 let number = 0;
 
 arr.forEach(item => {
-  const abob = item[0][0]
-  const aboba = item[0][1]
-  const lixur = item[1][0]
-  const licher = item[1][1]
+    const abob = item[0][0]
+    const aboba = item[0][1]
+    const lixur = item[1][0]
+    const licher = item[1][1]
 
-  if (abob >= lixur && aboba <= licher || abob <= lixur && aboba >= licher) {
-    number++;
-  }
+    if (abob >= lixur && aboba <= licher || abob <= lixur && aboba >= licher) {
+        number++;
+    }
 })
 
 console.log(number)
