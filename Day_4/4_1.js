b3774e7c5a6d7f46f573ba7c4494efea1b7995d7
ZaHaR <104730836+ZaHaRyK@users.noreply.github.com>
2023-05-04 17:28:57 +0300

    Add files via upload

---
+++ Day_4/4_1.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const arr = fs.readFileSync("req.txt", "utf8").split('\r\n').map(item => item.split(',').map(it => it.split('-').map(str => parseInt(str))));
+
+let number = 0;
+
+arr.forEach(item => {
+  const abob = item[0][0]
+  const aboba = item[0][1]
+  const lixur = item[1][0]
+  const licher = item[1][1]
+
+  if (abob >= lixur && aboba <= licher || abob <= lixur && aboba >= licher) {
+    number++;
+  }
+})
+
+console.log(number)
